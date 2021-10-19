e6ceb8d9e322f9e613a27b59eec26bea0e443549
Roscoe <elysiaroscoe@gmail.com>
2021-10-19 15:02:15 -0700

    commented out but added Marisa's answer for ref

--- PartII/script.js
+++ PartII/script.js
@@ -38,3 +38,19 @@ function addLike3() {
     count3Element.innerText = count3 + " like(s)"
     countArray[2] = count3
 }
+
+/* 
+MARISA's SOLUTION: MUCH CLEANER
+var likes = [9, 12, 9];
+var spans = [
+    document.querySelector("#post-1"),
+    document.querySelector("#post-2"),
+    document.querySelector("#post-3")
+];
+
+function like(id) {
+    likes[id]++;
+    spans[id].innerHTML = likes[id] + "like(s)";
+}
+
+*/
